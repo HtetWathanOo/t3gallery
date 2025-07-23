b8544f7ee1f46a0e19d7c2f4a02a9193330c43a7
Htet Wathan Oo <htetwathanoo17@gmail.com>
2025-07-23 14:16:35 +0630

    Actually linking the database to the UI

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ##TODO
 
--[x]Make it deploy 
--[x]Scaffold basic ui with mock data
--[]Actually set up a database
+-[x]Make it deploy -[x]Scaffold basic ui with mock data -[x]Actually set up a database
 -[]Attach database to UI
 -[]Add authentication
 -[]Add image upload
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,19 @@
 import { db } from "~/server/db";
 export const dynamic ="force-dynamic";
-const mockUrls= [
-  "https://1vusvrntsn.ufs.sh/f/UGqYrZEKlItDBr95Yj0lwuI2VfFEnYgNZ3SsCRxi8Jev40PQ",
-  "https://1vusvrntsn.ufs.sh/f/UGqYrZEKlItDcOEpow6uyCeRFlX9I7B35tYmkSHqV60pbEnD",
-  "https://1vusvrntsn.ufs.sh/f/UGqYrZEKlItDvpk1s9OKEoCrHFftqu2zkyMIecdNZSUVLsw9",
-"https://1vusvrntsn.ufs.sh/f/UGqYrZEKlItDbTqqI5H2BKRaUHSlfkWjc4FqXe1xdv8E9hCz"
-]
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,  url,
-}));
-export default async function HomePage() {
-  const posts= await db.query.posts.findMany();
 
-  console.log(posts);
-  return (
+export default async function HomePage() {
+  const images= await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id),
+  });
+
+return (
     <main className="">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id +"-"+ index} className="w-1/2 p-4">
+    
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id +"-"+ index} className="flex w-48 flex-col p-4">
             <img src={image.url} alt="image" className="w-full h-auto rounded-lg shadow-md"/>
+            <div> {image.name} </div>
           </div>
         ))}
       </div>
